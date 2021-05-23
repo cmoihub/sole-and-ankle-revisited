715f0dc9c3f0b688660b88d06970d53e78a14c0c
Craig Isesele <10642539+cmoihub@users.noreply.github.com>
2021-05-23 15:30:27 -0400

    exercise 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { ThemeProvider } from "styled-components";
+import { QUERIES } from "../../constants";
 
-import Header from '../Header';
-import ShoeIndex from '../ShoeIndex';
+import Header from "../Header";
+import ShoeIndex from "../ShoeIndex";
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+  const [sortId, setSortId] = React.useState("newest");
 
   return (
-    <>
+    <ThemeProvider theme={{ queries: QUERIES }}>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
       </Main>
-    </>
+    </ThemeProvider>
   );
 };
 
